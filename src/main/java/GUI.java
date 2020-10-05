7be07ab5af82fb0e97e337e1b590494159a67a60
Ryan Albertson <ryan061999@gmail.com>
2020-10-05 12:39:07 -0400

    Update GUI.java

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -24,6 +24,7 @@ public class GUI extends JFrame {
     public GUI() {
 
         // NEED TO RESCALE THE AXES AND CENTER THE ORIGIN IN THE CENTER
+        // CHANGE SCALING FOR EACH GRAPH SIZE
 
         frame = new JFrame();
         frame.setTitle("Algorithm Visualizer");
